4dce4926cf5c8d0b7cf4a59e4a7e9cb6c5506ea7
topepo <mxkuhn@gmail.com>
2020-05-18 14:31:55 -0400

    use simulated data for some tests

--- tests/testthat/test_discretize_tree.R
+++ tests/testthat/test_discretize_tree.R
@@ -1,18 +1,19 @@
-
 library(testthat)
 library(dplyr)
 library(rsample)
 
 context("discretize_tree")
 
-data("credit_data")
-data("okc")
+source(test_path("make_binned_data.R"))
 
-set.seed(42)
+# ------------------------------------------------------------------------------
+
+data("credit_data", package = "modeldata")
+data("okc", package = "modeldata")
 
 # Data for classification problem testing
+set.seed(42)
 credit_data_split <- initial_split(credit_data, strata = "Status")
-
 credit_data_train <- training(credit_data_split)
 credit_data_test <- testing(credit_data_split)
 
@@ -32,11 +33,14 @@ xgb_credit_test <- xgboost::xgb.DMatrix(
   label = ifelse(credit_data_test[["Status"]] == "bad", 0, 1)
 )
 
-# Data for regression problem testing (naive)
-okc_data_split <- initial_split(okc, strata = "age")
+# ------------------------------------------------------------------------------
 
+# Data for regression problem testing (naive)
+set.seed(1773)
+okc_data_split <- initial_split(okc, strata = "age")
 okc_data_train <- training(okc_data_split)
 okc_data_test <- testing(okc_data_split)
+
 
 rec_okc <- okc_data_train %>% 
   select(-age) %>% 
@@ -54,9 +58,11 @@ xgb_okc_test <- xgboost::xgb.DMatrix(
   label = okc_data_test[["age"]]
 )
 
+# ------------------------------------------------------------------------------
+
 test_that("run_xgboost for classification", {
   
-  xgboost <- run_xgboost(
+  xgboost <- embed:::run_xgboost(
     xgb_credit_train,
     xgb_credit_test,
     .learn_rate = 0.3,
@@ -75,7 +81,7 @@ test_that("run_xgboost for classification", {
 
 test_that("run_xgboost for regression", {
   
-  xgboost <- run_xgboost(
+  xgboost <- embed:::run_xgboost(
     xgb_okc_train,
     xgb_okc_test,
     .learn_rate = 0.3,
@@ -95,7 +101,7 @@ test_that("run_xgboost for regression", {
 test_that("xgb_binning for classification", {
   
   # Usual case
-  xgb_binning <- xgb_binning(
+  xgb_binning <- embed:::xgb_binning(
     credit_data_train,
     "Status",
     "Seniority",
@@ -103,7 +109,7 @@ test_that("xgb_binning for classification", {
     learn_rate = 0.3,
     num_breaks = 10,
     tree_depth = 1
-    )
+  )
   
   expect_output(print(xgb_binning))
   expect_equal(length(xgb_binning), 6)
@@ -114,7 +120,7 @@ test_that("xgb_binning for classification", {
   credit_data_na[1, "Status"] <- NA
   
   expect_error(
-    xgb_binning(
+    embed:::xgb_binning(
       credit_data_na,
       "Status",
       "Seniority",
@@ -132,7 +138,7 @@ test_that("xgb_binning for classification", {
   credit_data_levels[1, "Status"] <- "third_level"
   
   expect_error(
-    xgb_binning(
+    embed:::xgb_binning(
       credit_data_levels,
       "Status",
       "Seniority",
@@ -146,10 +152,11 @@ test_that("xgb_binning for classification", {
   )
   
   # Algorithm runs on a too small training set/ insufficient variation in data
+  set.seed(2393)
   credit_data_small <- dplyr::sample_n(credit_data_train, 20)
   
   expect_error(
-    xgb_binning(
+    embed:::xgb_binning(
       credit_data_small,
       "Status",
       "Seniority",
@@ -163,7 +170,7 @@ test_that("xgb_binning for classification", {
   
   # Insufficient datapoints in inner test set
   expect_error(
-    xgb_binning(
+    embed:::xgb_binning(
       credit_data_small,
       "Status",
       "Seniority",
@@ -180,7 +187,7 @@ test_that("xgb_binning for classification", {
 test_that("xgb_binning for regression", {
   
   # Usual case
-  xgb_binning <- xgb_binning(
+  xgb_binning <- embed:::xgb_binning(
     okc_data_train,
     "age",
     "height",
@@ -191,7 +198,7 @@ test_that("xgb_binning for regression", {
   )
   
   expect_output(print(xgb_binning))
-  expect_equal(length(xgb_binning), 6)
+  expect_equal(length(xgb_binning), 7)
   expect_type(xgb_binning, "double")
   
   # Target has missing values
@@ -199,7 +206,7 @@ test_that("xgb_binning for regression", {
   okc_data_na[1, "age"] <- NA
   
   expect_error(
-    xgb_binning(
+    embed:::xgb_binning(
       okc_data_na,
       "age",
       "height",
@@ -212,10 +219,11 @@ test_that("xgb_binning for regression", {
   )
   
   # Algorithm runs on a too small training set/ insufficient variation in data
+  set.seed(8134)
   okc_data_small <- dplyr::sample_n(okc_data_train, 10)
   
   expect_error(
-    xgb_binning(
+    embed:::xgb_binning(
       okc_data_small,
       "age",
       "height",
@@ -227,9 +235,9 @@ test_that("xgb_binning for regression", {
     "No splits could be calculated as there is inssuficient training data/ insufficient data variation for XgBoost. Consider increasing the prop parameter to devote more data to the inner training set."
   )
   
-  # Insufficient datapoints in inner test set
+  # Insufficient data points in inner test set
   expect_error(
-    xgb_binning(
+    embed:::xgb_binning(
       credit_data_small,
       "Status",
       "Seniority",
@@ -244,84 +252,40 @@ test_that("xgb_binning for regression", {
 })
 
 test_that("step_discretize_tree for classification", {
-  
+  set.seed(8497)
+  sim_tr <- sim_data_2class(1000)
+  sim_te <- sim_data_2class(100)
   # General use
   xgb_rec <-
-    recipe(Status ~ ., data = credit_data_train) %>%
-    step_medianimpute(all_numeric()) %>%
-    step_discretize_tree(all_numeric(), -Time, outcome = "Status")
+    recipe(class ~ ., data = sim_tr) %>%
+    step_discretize_tree(all_predictors(), outcome = "class")
   
-  xgb_rec <- prep(xgb_rec, training = credit_data_train)
+  set.seed(28)
+  xgb_rec <- prep(xgb_rec, training = sim_tr)
   
-  xgb_train_bins <- bake(xgb_rec, credit_data_train)
-  xgb_test_bins <- bake(xgb_rec, credit_data_test)
+  xgb_train_bins <- bake(xgb_rec, sim_tr)
+  xgb_test_bins <- bake(xgb_rec, sim_tr)
   
   expect_output(print(xgb_train_bins))
   expect_output(print(xgb_test_bins))
   expect_equal(
-    levels(xgb_train_bins$Seniority),
-    c("[-Inf,1.5)", "[1.5,2.5)", "[2.5,4)", "[4,9)", "[9,13)", "[13,18)", "[18, Inf]")
+    levels(xgb_train_bins$x),
+    c("[-Inf,0.3023)", "[0.3023,0.6815)", "[0.6815, Inf]")
   )
   expect_equal(
-    levels(xgb_train_bins$Age),
-    c("[-Inf,9.5)", "[9.5, Inf]")
+    levels(xgb_train_bins$y),
+    c("[-Inf,0.3212)", "[0.3212, Inf]")
+  )
+  
+  expect_equal(
+    levels(xgb_train_bins$x),
+    levels(xgb_test_bins$x)
   )
   expect_equal(
-    levels(xgb_train_bins$Expenses),
-    c("[-Inf,45.5)", "[45.5, Inf]")
+    levels(xgb_train_bins$z),
+    levels(xgb_test_bins$z)
   )
-  expect_equal(
-    levels(xgb_train_bins$Income),
-    c("[-Inf,112.5)", "[112.5, Inf]")
-  )
-  expect_equal(
-    levels(xgb_train_bins$Assets),
-    c("[-Inf,10)", "[10, Inf]")
-  )
-  expect_equal(
-    levels(xgb_train_bins$Debt),
-    c("[-Inf,67)", "[67, Inf]")
-  )
-  expect_equal(
-    levels(xgb_train_bins$Amount),
-    c("[-Inf,192)", "[192, Inf]")
-  )
-  expect_equal(
-    levels(xgb_train_bins$Price),
-    c("[-Inf,996)", "[996, Inf]")
-  )
-  expect_equal(
-    levels(xgb_train_bins$Seniority),
-    levels(xgb_test_bins$Seniority)
-  )
-  expect_equal(
-    levels(xgb_train_bins$Age),
-    levels(xgb_test_bins$Age)
-  )
-  expect_equal(
-    levels(xgb_train_bins$Expenses),
-    levels(xgb_test_bins$Expenses)
-  )
-  expect_equal(
-    levels(xgb_train_bins$Income),
-    levels(xgb_test_bins$Income)
-  )
-  expect_equal(
-    levels(xgb_train_bins$Assets),
-    levels(xgb_test_bins$Assets)
-  )
-  expect_equal(
-    levels(xgb_train_bins$Debt),
-    levels(xgb_test_bins$Debt)
-  )
-  expect_equal(
-    levels(xgb_train_bins$Amount),
-    levels(xgb_test_bins$Amount)
-  )
-  expect_equal(
-    levels(xgb_train_bins$Price),
-    levels(xgb_test_bins$Price)
-  )
+ 
   
   # No numeric variables present
   predictors_non_numeric <- c(
@@ -348,7 +312,7 @@ test_that("step_discretize_tree for classification", {
       step_medianimpute(all_numeric()) %>%
       step_discretize_tree(all_numeric(), outcome = "Status") %>%
       prep(retain = TRUE)
-    )
+  )
   
   expect_true(
     grepl(
@@ -360,32 +324,38 @@ test_that("step_discretize_tree for classification", {
 })
 
 test_that("step_discretize_tree for regression", {
-  
+  set.seed(8497)
+  sim_tr <- sim_data_reg(1000)
+  sim_te <- sim_data_reg(100)
   # General use
   xgb_rec <-
-    recipe(age ~ ., data = okc_data_train) %>%
-    step_medianimpute(all_numeric()) %>%
-    step_discretize_tree(all_numeric(), outcome = "age")
+    recipe(y ~ ., data = sim_tr) %>%
+    step_discretize_tree(all_predictors(), outcome = "y")
   
-  xgb_rec <- prep(xgb_rec, training = okc_data_train)
+  xgb_rec <- prep(xgb_rec, training = sim_tr)
   
-  xgb_train_bins <- bake(xgb_rec, okc_data_train)
-  xgb_test_bins <- bake(xgb_rec, okc_data_test)
+  xgb_train_bins <- bake(xgb_rec, sim_tr)
+  xgb_test_bins <- bake(xgb_rec, sim_tr)
   
   expect_output(print(xgb_train_bins))
   expect_output(print(xgb_test_bins))
   expect_equal(
-    levels(xgb_train_bins$Seniority),
-    c("[-Inf,1.5)", "[1.5,2.5)", "[2.5,4)", "[4,9)", "[9,13)", "[13,18)", "[18, Inf]")
+    levels(xgb_train_bins$x),
+    c("[-Inf,0.292)", "[0.292,0.4192)", "[0.4192,0.5551)", "[0.5551,0.6834)", "[0.6834, Inf]")
   )
   expect_equal(
-    levels(xgb_train_bins$height),
-    c("[-Inf,23.5)", "[23.5,25)", "[25,26.5)", "[26.5,28)", "[28,29.5)", "[29.5,41)", "[41, Inf]")
+    levels(xgb_train_bins$z),
+    c("[-Inf,0.07115)", "[0.07115,0.2061)", "[0.2061,0.3212)", "[0.3212,0.4386)", 
+      "[0.4386,0.569)", "[0.569,0.6808)", "[0.6808,0.7849)", "[0.7849,0.9201)", "[0.9201, Inf]")
   )
   expect_equal(
-    levels(xgb_train_bins$height),
-    levels(xgb_test_bins$height)
+    levels(xgb_train_bins$x),
+    levels(xgb_test_bins$x)
   )
+  expect_equal(
+    levels(xgb_train_bins$z),
+    levels(xgb_test_bins$z)
+  )  
   
   # No numeric variables present
   predictors_non_numeric <- c(
