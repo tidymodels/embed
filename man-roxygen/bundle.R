fac097664ca635d1054b9b07a3847c74eb301da8
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2022-09-23 11:17:07 -0700

    Update man-roxygen/bundle.R
    Co-authored-by: Julia Silge <julia.silge@gmail.com>

--- man-roxygen/bundle.R
+++ man-roxygen/bundle.R
@@ -1,5 +1,5 @@
 #' @section Saving prepped recipe object:
 #' 
-#' This recipe step may require native serialization methods to be properly 
-#' saved and/or passed between R sessions. To learn more about preparing 
-#' prepped recipes for serialization, see the bundle package.
+#' This recipe step may require native serialization when saving for 
+#' use in another R session. To learn more about serialization for 
+#' prepped recipes, see the [bundle](https://rstudio.github.io/bundle/) package.
