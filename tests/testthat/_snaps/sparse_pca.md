d813e6a875ec09d461a526f4e10caff8e9a2a278
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2023-03-21 10:32:57 -0700

    split up tests

--- tests/testthat/_snaps/sparse_pca.md
+++ tests/testthat/_snaps/sparse_pca.md
@@ -1,21 +1,3 @@
-# step_pca_sparse_bayes
-
-    Code
-      rec
-    Message
-      
-      -- Recipe ----------------------------------------------------------------------
-      
-      -- Inputs 
-      Number of variables by role
-      predictor: 56
-      
-      -- Training information 
-      Training data contained 1817 data points and no incomplete rows.
-      
-      -- Operations 
-      * Sparse PCA extraction with: angle_ch_1, area_ch_1, ... | Trained
-
 # step_pca_sparse
 
     Code
@@ -27,39 +9,6 @@
       -- Inputs 
       Number of variables by role
       predictor: 56
-      
-      -- Training information 
-      Training data contained 1817 data points and no incomplete rows.
-      
-      -- Operations 
-      * Sparse PCA extraction with: angle_ch_1, area_ch_1, ... | Trained
-
-# printing
-
-    Code
-      print_test
-    Message
-      
-      -- Recipe ----------------------------------------------------------------------
-      
-      -- Inputs 
-      Number of variables by role
-      predictor: 55
-      
-      -- Operations 
-      * No Sparse PCA components were extracted from: all_predictors()
-
----
-
-    Code
-      prep(print_test)
-    Message
-      
-      -- Recipe ----------------------------------------------------------------------
-      
-      -- Inputs 
-      Number of variables by role
-      predictor: 55
       
       -- Training information 
       Training data contained 1817 data points and no incomplete rows.
--- tests/testthat/test-pca_sparse_bayes.R
+++ tests/testthat/test-pca_sparse_bayes.R
@@ -12,27 +12,32 @@ te <- cells[split, ]
 
 # ------------------------------------------------------------------------------
 
-test_that("step_pca_sparse", {
-  skip_if_not_installed("irlba")
-
+test_that("step_pca_sparse_bayes", {
+  skip_if_not_installed("VBsparsePCA")
+  
   rec <-
     recipe(~., data = tr) %>%
-    step_pca_sparse(
+    step_pca_sparse_bayes(
       all_predictors(),
       num_comp = 4,
-      predictor_prop = 1 / 2
+      prior_slab_dispersion = 1 / 2,
+      prior_mixture_threshold = 1 / 15
     ) %>%
     prep()
-
-  direct_mod <- irlba::ssvd(as.matrix(tr), k = 4, n = ncol(tr) / 2)
-  direct_coef <- direct_mod$v
+  
+  direct_mod <- VBsparsePCA::VBsparsePCA(
+    as.matrix(tr),
+    lambda = 1 / 2,
+    r = 4,
+    threshold = 1 / 15
+  )
+  direct_coef <- svd(direct_mod$loadings)$u
   embed_coef <- rec$steps[[1]]$res
   vars <- rownames(embed_coef)
   dimnames(embed_coef) <- NULL
-  dimnames(direct_coef) <- NULL
-
+  
   expect_equal(abs(direct_coef), abs(embed_coef), tolerance = 0.1)
-
+  
   tidy_coef <- tidy(rec, number = 1)
   # test a few values
   expect_equal(
@@ -41,25 +46,52 @@ test_that("step_pca_sparse", {
     ],
     embed_coef[which(vars == "angle_ch_1"), 1]
   )
-
+  
   expect_equal(
     tidy_coef$value[
       tidy_coef$terms == "total_inten_ch_3" & tidy_coef$component == "PC3"
     ],
     embed_coef[which(vars == "total_inten_ch_3"), 3]
   )
-
+  
   expect_snapshot(rec)
 })
 
 # ------------------------------------------------------------------------------
 
+test_that("bake method errors when needed non-standard role columns are missing", {
+  rec <- recipe(~., data = tr) %>%
+    step_pca_sparse_bayes(
+      avg_inten_ch_1, avg_inten_ch_2, avg_inten_ch_3, avg_inten_ch_4,
+      num_comp = 2,
+      prior_slab_dispersion = 1 / 2,
+      prior_mixture_threshold = 1 / 15
+    ) %>%
+    update_role(avg_inten_ch_1, new_role = "potato") %>%
+    update_role_requirements(role = "potato", bake = FALSE)
+  
+  rec_trained <- prep(rec, training = tr, verbose = FALSE)
+  
+  expect_error(
+    bake(rec_trained, new_data = tr[, -3]),
+    class = "new_data_missing_column"
+  )
+})
+
+test_that("printing", {
+  print_test <- recipe(~., data = tr[, -5]) %>%
+    step_pca_sparse_bayes(all_predictors())
+  expect_snapshot(print_test)
+  expect_snapshot(prep(print_test))
+})
+
+# ------------------------------------------------------------------------------
+
 test_that("empty selections", {
-  data(ad_data, package = "modeldata")
   expect_error(
     rec <-
       recipe(Class ~ Genotype + tau, data = ad_data) %>%
-      step_pca_sparse(starts_with("potato")) %>%
+      step_pca_sparse_bayes(starts_with("potato")) %>%
       prep(),
     regexp = NA
   )
